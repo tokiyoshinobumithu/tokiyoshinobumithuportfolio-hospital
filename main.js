e90b4115041f8e1ae803394fbfd4a6ca958e9a5a
tokiyoshinobumithu <109967641+tokiyoshinobumithu@users.noreply.github.com>
2022-10-24 17:18:30 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,43 @@
+$(function (){
+    $('.slider').slick ({
+        arrows: false,
+        fdots: true,
+        vertical: true,
+        autoplay: true,
+        autoplaySpeed:4000,
+        speed: 2000,
+        pauseOnFocus: false,
+        pauseOnHover: false
+    })
+})
+
+$(function (){
+    var width = $(window).width();
+    $('.news-list-item > .title, .open-btn').on('click',function (){
+        var parentElem = $(this).parent();
+        $(parentElem).toggleClass('active');
+        $(parentElem).children('.has-chiled').stop().slideToggle(400);
+        return false;
+    });
+});
+
+$(function () {
+    $('.navi-btn').on('click',function () {
+        $('.navi-btn, .menu-open').toggleClass('open');
+    });
+});
+
+$(function () {
+    $('.page_top-btn').click(function (){
+        $('body, html').animate( {
+            scrollTop:0
+        },500
+        );
+    });
+});
+
+$(function () {
+    $('.down-menu-name').on('mouseover',function () {
+        $('.down-menu').addClass('slide-down');
+    });
+});
