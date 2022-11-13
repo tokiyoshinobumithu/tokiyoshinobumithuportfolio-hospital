a403e1a22a3fe2d07663ec1dfea0676357810162
tokiyoshinobumithu <109967641+tokiyoshinobumithu@users.noreply.github.com>
2022-11-14 00:47:13 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
+$(function(){
+    $(window).on('load',function(){
+      $('.three-quarter-spinner').delay(500).fadeOut(500);
+      $('.spinner-box').delay(800).fadeOut(900);
+  });
+});
 $(function (){
-    $('.slider').slick ({
-        arrows: false,
-        fdots: true,
-        vertical: true,
-        autoplay: true,
-        autoplaySpeed:4000,
-        speed: 2000,
-        pauseOnFocus: false,
-        pauseOnHover: false
-    })
-})
-
+    setTimeout(function(){
+        $('.slider').slick ({
+            arrows: false,
+            fdots: true,
+            vertical: true,
+            autoplay: true,
+            autoplaySpeed:4000,
+            speed: 2000,
+            pauseOnFocus: false,
+            pauseOnHover: false
+        });
+    }, 3500);
+});
 $(function (){
     var width = $(window).width();
     $('.news-list-item > .title, .open-btn').on('click',function (){
